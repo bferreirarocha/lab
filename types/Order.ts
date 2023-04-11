010694502adffabbe134d936e6110c28e0f2f418
Bruno Ferreira <ferreirarocha.br@gmail.com>
2023-04-11 09:26:22 +0200

    First  commit

---
+++ types/Order.ts
@@ -0,0 +1,9 @@
+//   type Actors = Database['public']['tables']['actors']['row']
+//   type MoviesResponse = Awaited<ReturnType<typeof getMovies>>
+//   type MoviesResponseSuccess = MoviesResponse['data'] & {
+//   actors: Actors[]
+//   }
+
+//  export type MoviesResponse = Awaited<ReturnType<typeof getMovies>>
+//  export type MoviesResponseSuccess = MoviesResponse['data']
+//  export type MoviesResponseError = MoviesResponse['error']
